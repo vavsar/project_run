85968d03bc93939d1cb3692894d5fd4c30b3a9c2
a.vostrikov <sarcigar@gmail.com>
2026-08-08 17:26:44 +0300

    add view company_details

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.conf import settings
 
-# Create your views here.
+
+@api_view(['GET'])
+def company_details(request):
+    return Response({
+        'company_name': f'{settings.COMPANY_NAME}',
+        'slogan': f'{settings.SLOGAN}',
+        'contacts': f'{settings.CONTACTS}',
+    })
--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -114,3 +114,7 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPANY_NAME = 'Придумай название сам'
+SLOGAN = 'Придумай слоган сам'
+CONTACTS = 'Придумай контакты сам'
